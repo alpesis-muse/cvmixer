f32dd4b1c9bace265a9df04d676736f98db1c725
K <kwailamchan@hotmail.com>
2018-01-25 01:22:56 +0800

    [master] backup the changes in operators. 	new file:   milkyway.jpg 	modified:   operators.c 	new file:   surfer.jpg

--- cvc/operators.c
+++ cvc/operators.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
-
-#include <cv.h>  
-#include <highgui.h>  
+#include <cv.h>
+#include <highgui.h>
  
+int main( int argc, char **argv ){
+    IplImage *surfer, *milkyway, *result;
+    int key = 0;
+    CvSize size;
  
-int main(int argc, char** argv)  
-{  
-    IplImage * src1 = cvLoadImage("1.jpg");
-    IplImage * src2 = cvLoadImage("3.jpg");  
-    IplImage * src3 = cvLoadImage("4.jpg");  
-    cvAdd(src1, src2, src2); 
-    cvShowImage( "测试1", src1);  
-    cvShowImage( "测试2", src2);  
-    cvShowImage( "测试3", src3);  
-    cvWaitKey(0);  
-    return 0;  
-} 
+    /* load images, check, get size (both should have the same) */
+    surfer = cvLoadImage("surfer.jpg", CV_LOAD_IMAGE_COLOR);
+    milkyway = cvLoadImage("milkyway.jpg", CV_LOAD_IMAGE_COLOR);
+    if((!surfer)||(!milkyway)){
+        printf("Could not open one or more images.");
+        exit -1;
+    }
+    size = cvGetSize(surfer);
+ 
+    /* create a empty image, same size, depth and channels of others */
+    result = cvCreateImage(size, surfer->depth, surfer->nChannels);
+    cvZero(result);
+ 
+    /* result = surfer + milkyway (NULL mask)*/
+    cvAdd(surfer, milkyway, result, NULL);
+ 
+    /* create a window, display the result, wait for a key */
+    cvNamedWindow("example", CV_WINDOW_AUTOSIZE);
+    cvShowImage("example", result);
+    cvWaitKey(0);
+ 
+    /* free memory and get out */
+    cvDestroyWindow("example");
+    cvReleaseImage(&surfer);
+    cvReleaseImage(&milkyway);
+    cvReleaseImage(&result);
+    return 0;
+}
